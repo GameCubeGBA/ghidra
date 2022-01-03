64ace97924dc6726e0e2abbc83262e003a439263
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-01-03 13:01:43 -0500

    Fix build

--- Ghidra/Framework/SoftwareModeling/src/test/java/ghidra/util/MultiIteratorTest.java
+++ Ghidra/Framework/SoftwareModeling/src/test/java/ghidra/util/MultiIteratorTest.java
@@ -18,11 +18,7 @@ package ghidra.util;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 import org.junit.Assert;
 import org.junit.Test;
