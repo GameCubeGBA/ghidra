c997adfc4f131781ace6255ff200c32883f3b266
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-01-05 22:59:44 -0500

    D

--- Ghidra/Features/Base/src/main/java/ghidra/test/processors/support/ProcessorEmulatorTestAdapter.java
+++ Ghidra/Features/Base/src/main/java/ghidra/test/processors/support/ProcessorEmulatorTestAdapter.java
@@ -1284,7 +1284,7 @@ public abstract class ProcessorEmulatorTestAdapter extends TestCase implements E
 				continue;
 			}
 			Symbol s =
-				SymbolUtilities.getExpectedLabelOrFunctionSymbol(program, name, m -> m == true); // error ignored
+			SymbolUtilities.getExpectedLabelOrFunctionSymbol(program, name, m -> m.toString()); // error ignored
 			if (s != null) {
 				return s;
 			}
