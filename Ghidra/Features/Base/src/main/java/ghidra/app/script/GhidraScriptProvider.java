59361f3ba19f9935ab03a63656993e517d422715
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-01-30 21:57:03 -0500

    Fix4

--- Ghidra/Features/Base/src/main/java/ghidra/app/script/GhidraScriptProvider.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/script/GhidraScriptProvider.java
@@ -17,6 +17,7 @@ package ghidra.app.script;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.util.regex.Pattern;
 
 import generic.jar.ResourceFile;
