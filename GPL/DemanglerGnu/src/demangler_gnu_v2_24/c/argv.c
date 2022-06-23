d0c90ccd07c34c5af5420181fc9c4bca351b0579
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-06-23 12:11:36 -0400

    Fix

--- GPL/DemanglerGnu/src/demangler_gnu_v2_24/c/argv.c
+++ GPL/DemanglerGnu/src/demangler_gnu_v2_24/c/argv.c
@@ -23,7 +23,6 @@ License along with libiberty; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 Boston, MA 02110-1301, USA.  */
 
-
 /*  Create and destroy argument vectors.  An argument vector is simply an
     array of string pointers, terminated by a NULL pointer. */
 
@@ -49,8 +48,7 @@ Boston, MA 02110-1301, USA.  */
 #define EOS '\0'
 #endif
 
-#define INITIAL_MAXARGC 8	/* Number of args + NULL in initial argv */
-
+#define INITIAL_MAXARGC 8 /* Number of args + NULL in initial argv */
 
 /*
 
@@ -67,25 +65,26 @@ argument vector.
 */
 
 char **
-dupargv (char **argv)
+dupargv(char **argv)
 {
   int argc;
   char **copy;
-  
+
   if (argv == NULL)
     return NULL;
-  
+
   /* the vector */
-  for (argc = 0; argv[argc] != NULL; argc++);
-  copy = (char **) xmalloc ((argc + 1) * sizeof (char *));
+  for (argc = 0; argv[argc] != NULL; argc++)
+    ;
+  copy = (char **)xmalloc((argc + 1) * sizeof(char *));
 
   /* the strings */
   for (argc = 0; argv[argc] != NULL; argc++)
-    {
-      int len = strlen (argv[argc]);
-      copy[argc] = (char *) xmalloc (len + 1);
-      strcpy (copy[argc], argv[argc]);
-    }
+  {
+    int len = strlen(argv[argc]);
+    copy[argc] = (char *)xmalloc(len + 1);
+    strcpy(copy[argc], argv[argc]);
+  }
   copy[argc] = NULL;
   return copy;
 }
@@ -103,36 +102,41 @@ itself.
 
 */
 
-void freeargv (char **vector)
+void freeargv(char **vector)
 {
   register char **scan;
 
   if (vector != NULL)
+  {
+    for (scan = vector; *scan != NULL; scan++)
     {
-      for (scan = vector; *scan != NULL; scan++)
-	{
-	  free (*scan);
-	}
-      free (vector);
+      free(*scan);
     }
+    free(vector);
+  }
 }
 
 static void
-consume_whitespace (const char **input)
+consume_whitespace(const char **input)
 {
-  while (ISSPACE (**input))
-    {
-      (*input)++;
-    }
+  while (ISSPACE(**input))
+  {
+    (*input)++;
+  }
 }
 
 static int
-only_whitespace (const char* input)
+only_whitespace(const char *input)
 {
-  while (*input != EOS && ISSPACE (*input))
-    input++;
 
-  return (*input == EOS);
+  while (*input != EOS)
+  {
+    if (ISSPACE(*input))
+      return 0;
+    input++;
+  }
+
+  return 1;
 }
 
 /*
@@ -175,7 +179,7 @@ returned, as appropriate.
 
 */
 
-char **buildargv (const char *input)
+char **buildargv(const char *input)
 {
   char *arg;
   char *copybuf;
@@ -188,101 +192,100 @@ char **buildargv (const char *input)
   char **nargv;
 
   if (input != NULL)
+  {
+    copybuf = (char *)xmalloc(strlen(input) + 1);
+    /* Is a do{}while to always execute the loop once.  Always return an
+ argv, even for null strings.  See NOTES above, test case below. */
+    do
     {
-      copybuf = (char *) xmalloc (strlen (input) + 1);
-      /* Is a do{}while to always execute the loop once.  Always return an
-	 argv, even for null strings.  See NOTES above, test case below. */
-      do
-	{
-	  /* Pick off argv[argc] */
-	  consume_whitespace (&input);
+      /* Pick off argv[argc] */
+      consume_whitespace(&input);
 
-	  if ((maxargc == 0) || (argc >= (maxargc - 1)))
-	    {
-	      /* argv needs initialization, or expansion */
-	      if (argv == NULL)
-		{
-		  maxargc = INITIAL_MAXARGC;
-		  nargv = (char **) xmalloc (maxargc * sizeof (char *));
-		}
-	      else
-		{
-		  maxargc *= 2;
-		  nargv = (char **) xrealloc (argv, maxargc * sizeof (char *));
-		}
-	      argv = nargv;
-	      argv[argc] = NULL;
-	    }
-	  /* Begin scanning arg */
-	  arg = copybuf;
-	  while (*input != EOS)
-	    {
-	      if (ISSPACE (*input) && !squote && !dquote && !bsquote)
-		{
-		  break;
-		}
-	      else
-		{
-		  if (bsquote)
-		    {
-		      bsquote = 0;
-		      *arg++ = *input;
-		    }
-		  else if (*input == '\\')
-		    {
-		      bsquote = 1;
-		    }
-		  else if (squote)
-		    {
-		      if (*input == '\'')
-			{
-			  squote = 0;
-			}
-		      else
-			{
-			  *arg++ = *input;
-			}
-		    }
-		  else if (dquote)
-		    {
-		      if (*input == '"')
-			{
-			  dquote = 0;
-			}
-		      else
-			{
-			  *arg++ = *input;
-			}
-		    }
-		  else
-		    {
-		      if (*input == '\'')
-			{
-			  squote = 1;
-			}
-		      else if (*input == '"')
-			{
-			  dquote = 1;
-			}
-		      else
-			{
-			  *arg++ = *input;
-			}
-		    }
-		  input++;
-		}
-	    }
-	  *arg = EOS;
-	  argv[argc] = xstrdup (copybuf);
-	  argc++;
-	  argv[argc] = NULL;
+      if ((maxargc == 0) || (argc >= (maxargc - 1)))
+      {
+        /* argv needs initialization, or expansion */
+        if (argv == NULL)
+        {
+          maxargc = INITIAL_MAXARGC;
+          nargv = (char **)xmalloc(maxargc * sizeof(char *));
+        }
+        else
+        {
+          maxargc *= 2;
+          nargv = (char **)xrealloc(argv, maxargc * sizeof(char *));
+        }
+        argv = nargv;
+        argv[argc] = NULL;
+      }
+      /* Begin scanning arg */
+      arg = copybuf;
+      while (*input != EOS)
+      {
+        if (ISSPACE(*input) && !squote && !dquote && !bsquote)
+        {
+          break;
+        }
+        else
+        {
+          if (bsquote)
+          {
+            bsquote = 0;
+            *arg++ = *input;
+          }
+          else if (*input == '\\')
+          {
+            bsquote = 1;
+          }
+          else if (squote)
+          {
+            if (*input == '\'')
+            {
+              squote = 0;
+            }
+            else
+            {
+              *arg++ = *input;
+            }
+          }
+          else if (dquote)
+          {
+            if (*input == '"')
+            {
+              dquote = 0;
+            }
+            else
+            {
+              *arg++ = *input;
+            }
+          }
+          else
+          {
+            if (*input == '\'')
+            {
+              squote = 1;
+            }
+            else if (*input == '"')
+            {
+              dquote = 1;
+            }
+            else
+            {
+              *arg++ = *input;
+            }
+          }
+          input++;
+        }
+      }
+      *arg = EOS;
+      argv[argc] = xstrdup(copybuf);
+      argc++;
+      argv[argc] = NULL;
 
-	  consume_whitespace (&input);
-	}
-      while (*input != EOS);
+      consume_whitespace(&input);
+    } while (*input != EOS);
 
-      free (copybuf);
-    }
+    free(copybuf);
+  }
   return (argv);
 }
 
@@ -298,47 +301,40 @@ if an error occurred while writing to FILE.
 
 */
 
-int
-writeargv (char **argv, FILE *f)
+int writeargv(char **argv, FILE *f)
 {
-  int status = 0;
-
   if (f == NULL)
     return 1;
 
   while (*argv != NULL)
+  {
+    const char *arg = *argv;
+
+    while (*arg != EOS)
     {
-      const char *arg = *argv;
+      char c = *arg;
 
-      while (*arg != EOS)
+      if (ISSPACE(c) || c == '\\' || c == '\'' || c == '"')
+        if (EOF == fputc('\\', f))
         {
-          char c = *arg;
-
-          if (ISSPACE(c) || c == '\\' || c == '\'' || c == '"')
-            if (EOF == fputc ('\\', f))
-              {
-                status = 1;
-                goto done;
-              }
-
-          if (EOF == fputc (c, f))
-            {
-              status = 1;
-              goto done;
-            }
-          arg++;
+          return 1;
         }
 
-      if (EOF == fputc ('\n', f))
-        {
-          status = 1;
-          goto done;
-        }
-      argv++;
+      if (EOF == fputc(c, f))
+      {
+        return 1;
+      }
+      arg++;
     }
 
- done:
-  return status;
+    if (EOF == fputc('\n', f))
+    {
+      return 1;
+    }
+    argv++;
+  }
+
+  return 0;
 }
 
 /*
@@ -364,8 +360,7 @@ operating system to free the memory when the program exits.
 
 */
 
-void
-expandargv (int *argcp, char ***argvp)
+void expandargv(int *argcp, char ***argvp)
 {
   /* The argument we are currently processing.  */
   int i = 0;
@@ -377,97 +372,97 @@ expandargv (int *argcp, char ***argvp)
   /* Loop over the arguments, handling response files.  We always skip
      ARGVP[0], as that is the name of the program being run.  */
   while (++i < *argcp)
+  {
+    /* The name of the response file.  */
+    const char *filename;
+    /* The response file.  */
+    FILE *f;
+    /* An upper bound on the number of characters in the response
+ file.  */
+    long pos;
+    /* The number of characters in the response file, when actually
+ read.  */
+    size_t len;
+    /* A dynamically allocated buffer used to hold options read from a
+ response file.  */
+    char *buffer;
+    /* Dynamically allocated storage for the options read from the
+ response file.  */
+    char **file_argv;
+    /* The number of options read from the response file, if any.  */
+    size_t file_argc;
+    /* We are only interested in options of the form "@file".  */
+    filename = (*argvp)[i];
+    if (filename[0] != '@')
+      continue;
+    /* If we have iterated too many times then stop.  */
+    if (--iteration_limit == 0)
     {
-      /* The name of the response file.  */
-      const char *filename;
-      /* The response file.  */
-      FILE *f;
-      /* An upper bound on the number of characters in the response
-	 file.  */
-      long pos;
-      /* The number of characters in the response file, when actually
-	 read.  */
-      size_t len;
-      /* A dynamically allocated buffer used to hold options read from a
-	 response file.  */
-      char *buffer;
-      /* Dynamically allocated storage for the options read from the
-	 response file.  */
-      char **file_argv;
-      /* The number of options read from the response file, if any.  */
-      size_t file_argc;
-      /* We are only interested in options of the form "@file".  */
-      filename = (*argvp)[i];
-      if (filename[0] != '@')
-	continue;
-      /* If we have iterated too many times then stop.  */
-      if (-- iteration_limit == 0)
-	{
-	  fprintf (stderr, "%s: error: too many @-files encountered\n", (*argvp)[0]);
-	  xexit (1);
-	}
-      /* Read the contents of the file.  */
-      f = fopen (++filename, "r");
-      if (!f)
-	continue;
-      if (fseek (f, 0L, SEEK_END) == -1)
-	goto error;
-      pos = ftell (f);
-      if (pos == -1)
-	goto error;
-      if (fseek (f, 0L, SEEK_SET) == -1)
-	goto error;
-      buffer = (char *) xmalloc (pos * sizeof (char) + 1);
-      len = fread (buffer, sizeof (char), pos, f);
-      if (len != (size_t) pos
-	  /* On Windows, fread may return a value smaller than POS,
-	     due to CR/LF->CR translation when reading text files.
-	     That does not in-and-of itself indicate failure.  */
-	  && ferror (f))
-	goto error;
-      /* Add a NUL terminator.  */
-      buffer[len] = '\0';
-      /* If the file is empty or contains only whitespace, buildargv would
-	 return a single empty argument.  In this context we want no arguments,
-	 instead.  */
-      if (only_whitespace (buffer))
-	{
-	  file_argv = (char **) xmalloc (sizeof (char *));
-	  file_argv[0] = NULL;
-	}
-      else
-	/* Parse the string.  */
-	file_argv = buildargv (buffer);
-      /* If *ARGVP is not already dynamically allocated, copy it.  */
-      if (!argv_dynamic)
-	*argvp = dupargv (*argvp);
-      /* Count the number of arguments.  */
-      file_argc = 0;
-      while (file_argv[file_argc])
-	++file_argc;
-      /* Now, insert FILE_ARGV into ARGV.  The "+1" below handles the
-	 NULL terminator at the end of ARGV.  */ 
-      *argvp = ((char **) 
-		xrealloc (*argvp, 
-			  (*argcp + file_argc + 1) * sizeof (char *)));
-      memmove (*argvp + i + file_argc, *argvp + i + 1, 
-	       (*argcp - i) * sizeof (char *));
-      memcpy (*argvp + i, file_argv, file_argc * sizeof (char *));
-      /* The original option has been replaced by all the new
-	 options.  */
-      *argcp += file_argc - 1;
-      /* Free up memory allocated to process the response file.  We do
-	 not use freeargv because the individual options in FILE_ARGV
-	 are now in the main ARGV.  */
-      free (file_argv);
-      free (buffer);
-      /* Rescan all of the arguments just read to support response
-	 files that include other response files.  */
-      --i;
-    error:
-      /* We're all done with the file now.  */
-      fclose (f);
+      fprintf(stderr, "%s: error: too many @-files encountered\n", (*argvp)[0]);
+      xexit(1);
     }
+    /* Read the contents of the file.  */
+    f = fopen(++filename, "r");
+    if (!f)
+      continue;
+    if (fseek(f, 0L, SEEK_END) == -1)
+      goto error;
+    pos = ftell(f);
+    if (pos == -1)
+      goto error;
+    if (fseek(f, 0L, SEEK_SET) == -1)
+      goto error;
+    buffer = (char *)xmalloc(pos * sizeof(char) + 1);
+    len = fread(buffer, sizeof(char), pos, f);
+    if (len != (size_t)pos
+        /* On Windows, fread may return a value smaller than POS,
+           due to CR/LF->CR translation when reading text files.
+           That does not in-and-of itself indicate failure.  */
+        && ferror(f))
+      goto error;
+    /* Add a NUL terminator.  */
+    buffer[len] = '\0';
+    /* If the file is empty or contains only whitespace, buildargv would
+ return a single empty argument.  In this context we want no arguments,
+ instead.  */
+    if (only_whitespace(buffer))
+    {
+      file_argv = (char **)xmalloc(sizeof(char *));
+      file_argv[0] = NULL;
+    }
+    else
+      /* Parse the string.  */
+      file_argv = buildargv(buffer);
+    /* If *ARGVP is not already dynamically allocated, copy it.  */
+    if (!argv_dynamic)
+      *argvp = dupargv(*argvp);
+    /* Count the number of arguments.  */
+    file_argc = 0;
+    while (file_argv[file_argc])
+      ++file_argc;
+    /* Now, insert FILE_ARGV into ARGV.  The "+1" below handles the
+ NULL terminator at the end of ARGV.  */
+    *argvp = ((char **)
+                  xrealloc(*argvp,
+                           (*argcp + file_argc + 1) * sizeof(char *)));
+    memmove(*argvp + i + file_argc, *argvp + i + 1,
+            (*argcp - i) * sizeof(char *));
+    memcpy(*argvp + i, file_argv, file_argc * sizeof(char *));
+    /* The original option has been replaced by all the new
+ options.  */
+    *argcp += file_argc - 1;
+    /* Free up memory allocated to process the response file.  We do
+ not use freeargv because the individual options in FILE_ARGV
+ are now in the main ARGV.  */
+    free(file_argv);
+    free(buffer);
+    /* Rescan all of the arguments just read to support response
+ files that include other response files.  */
+    --i;
+  error:
+    /* We're all done with the file now.  */
+    fclose(f);
+  }
 }
 
 /*
@@ -481,8 +476,7 @@ Returns zero if @var{argv} is NULL.
 
 */
 
-int
-countargv (char **argv)
+int countargv(char **argv)
 {
   int argc;
 
@@ -498,48 +492,46 @@ countargv (char **argv)
 /* Simple little test driver. */
 
 static const char *const tests[] =
-{
-  "a simple command line",
-  "arg 'foo' is single quoted",
-  "arg \"bar\" is double quoted",
-  "arg \"foo bar\" has embedded whitespace",
-  "arg 'Jack said \\'hi\\'' has single quotes",
-  "arg 'Jack said \\\"hi\\\"' has double quotes",
-  "a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9",
-  
-  /* This should be expanded into only one argument.  */
-  "trailing-whitespace ",
+    {
+        "a simple command line",
+        "arg 'foo' is single quoted",
+        "arg \"bar\" is double quoted",
+        "arg \"foo bar\" has embedded whitespace",
+        "arg 'Jack said \\'hi\\'' has single quotes",
+        "arg 'Jack said \\\"hi\\\"' has double quotes",
+        "a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9",
 
-  "",
-  NULL
-};
+        /* This should be expanded into only one argument.  */
+        "trailing-whitespace ",
 
-int
-main (void)
+        "",
+        NULL};
+
+int main(void)
 {
   char **argv;
   const char *const *test;
   char **targs;
 
   for (test = tests; *test != NULL; test++)
+  {
+    printf("buildargv(\"%s\")\n", *test);
+    if ((argv = buildargv(*test)) == NULL)
     {
-      printf ("buildargv(\"%s\")\n", *test);
-      if ((argv = buildargv (*test)) == NULL)
-	{
-	  printf ("failed!\n\n");
-	}
-      else
-	{
-	  for (targs = argv; *targs != NULL; targs++)
-	    {
-	      printf ("\t\"%s\"\n", *targs);
-	    }
-	  printf ("\n");
-	}
-      freeargv (argv);
+      printf("failed!\n\n");
     }
+    else
+    {
+      for (targs = argv; *targs != NULL; targs++)
+      {
+        printf("\t\"%s\"\n", *targs);
+      }
+      printf("\n");
+    }
+    freeargv(argv);
+  }
 
   return 0;
 }
 
-#endif	/* MAIN */
+#endif /* MAIN */
